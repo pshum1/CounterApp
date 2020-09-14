eeb89c8a329995d6266c05f19b246d52936efcde
Pierre Shum <pshum1@hawk.iit.edu>
2020-09-14 12:13:14 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, {useState} from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="+"
+        onPress={() => {
+          let count = counter +1
+          setCounter(count);
+        }}
+      />
+
+      <Text>Number: {counter}</Text>
+
+      <Button
+        title="-"
+        onPress={() => {
+          let count = counter - 1
+          setCounter(count);
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
